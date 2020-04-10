39e75c22b3015c82307505633eb23f3ca524f60c
marissaverdonck <marissa.verdonck@hva.nl>
2020-04-10 12:18:08 +0200

    game

--- server.js
+++ server.js
@@ -5,25 +5,42 @@ var io = require('socket.io')(http);
 var path = require('path')
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.static('static'));
-// app.set('view engine', 'ejs');
-// app.set('views', 'views');
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
+// connection event from a browser
 io.on('connection', function(socket) {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
+  socket.broadcast.emit('newUser', socket.id);
+
+  // disconnection event from a browser
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
 
-  socket.on('chat message', function(msg) {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  // Lisen for event 'setUsername' from the client
+  socket.on('setUsername', function(data) {
+    socket.username = data;
+    io.emit('setUsername', data.id, data.username); //Sends message to all sockets
+  });
+
+  // Lisen for event 'chat message' from the client
+  socket.on('setMessage', function(data) {
+    if (socket.username) {
+      io.emit('setMessage', data.message, socket.username.username);
+    } else { io.emit('setMessage', data.message) }
+  });
+
+  // Lisen for event 'hangman' from the client
+  socket.on('hangman', function(data) {
+    if (socket.username) {
+      io.emit('hangman', data.hangman, socket.username.username);
+    } else {
+      io.emit('hangman', data.hangman)
+    }
   });
 });
 
